e798f0bba3d57c45091173aa3c931c68223abdaf
cotenoni <nicolas.cotenolin@gmail.com>
2021-05-21 16:00:18 -0400

    howSum refactoring

--- memoization/howSum.py
+++ memoization/howSum.py
@@ -2,10 +2,7 @@ from typing import List
 
 
 class Solution:
-    def howSum(self, target:int, numbers: List[int], values = None) -> List[int]:
-        if values == None:
-            values = []
-
+    def howSum(self, target:int, numbers: List[int]) -> List[int]:
         if target == 0:
             return []
         
@@ -14,16 +11,18 @@ class Solution:
         
         for n in numbers:
             remainder = target - n
-            if self.howSum(remainder, numbers, values) != None:
-                values.append(n)
-                return values
+            remainderResult = self.howSum(remainder, numbers)
+            
+            if remainderResult != None:
+                remainderResult.append(n)
+                return remainderResult
 
         return None
 
         
 solution = Solution()
-print(solution.howSum(7, [2, 3])) #expected: true
-print(solution.howSum(7, [5, 3, 4, 7])) #expected: true
-print(solution.howSum(7, [2, 4])) #expected: false
-print(solution.howSum(8, [2, 3, 5])) #expected: true
-#print(solution.howSum(300, [7, 14])) #expected: false
+print(solution.howSum(7, [2, 3])) #expected: [3, 2, 2]
+print(solution.howSum(7, [5, 3, 4, 7])) #expected: [4, 3]
+print(solution.howSum(7, [2, 4])) #expected: None
+print(solution.howSum(8, [2, 3, 5])) #expected: [2, 2, 2, 2]
+#print(solution.howSum(300, [7, 14])) #expected: None
