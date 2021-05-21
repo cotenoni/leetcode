732254585a5b208e1e132d54008e6acb7de40666
cotenoni <nicolas.cotenolin@gmail.com>
2021-05-21 16:04:10 -0400

    howSum with memoization

--- memoization/howSum.py
+++ memoization/howSum.py
@@ -2,7 +2,13 @@ from typing import List
 
 
 class Solution:
-    def howSum(self, target:int, numbers: List[int]) -> List[int]:
+    def howSum(self, target:int, numbers: List[int], memo = None) -> List[int]:
+        if memo == None:
+            memo = {}
+
+        if target in memo:
+            return memo[target]
+
         if target == 0:
             return []
         
@@ -11,12 +17,14 @@ class Solution:
         
         for n in numbers:
             remainder = target - n
-            remainderResult = self.howSum(remainder, numbers)
-            
+            remainderResult = self.howSum(remainder, numbers, memo)
+
             if remainderResult != None:
                 remainderResult.append(n)
+                memo[target] = remainderResult
                 return remainderResult
 
+        memo[target] = None
         return None
 
         
@@ -25,4 +33,4 @@ print(solution.howSum(7, [2, 3])) #expected: [3, 2, 2]
 print(solution.howSum(7, [5, 3, 4, 7])) #expected: [4, 3]
 print(solution.howSum(7, [2, 4])) #expected: None
 print(solution.howSum(8, [2, 3, 5])) #expected: [2, 2, 2, 2]
-#print(solution.howSum(300, [7, 14])) #expected: None
+print(solution.howSum(300, [7, 14])) #expected: None
