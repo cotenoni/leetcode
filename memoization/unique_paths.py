4c63b4683afc353c02961f7b7439195cfe4be347
cotenoni <nicolas.cotenolin@gmail.com>
2021-05-21 13:39:57 -0400

    Unique Paths refactoring (forgot to pass memo down)

--- memoization/unique_paths.py
+++ memoization/unique_paths.py
@@ -10,7 +10,7 @@ class Solution:
         if m == 0 or n == 0:
             return 0
 
-        memo[(m, n)] = self.uniquePaths(m - 1, n) + self.uniquePaths(m, n - 1)
+        memo[(m, n)] = self.uniquePaths(m - 1, n, memo) + self.uniquePaths(m, n - 1, memo)
         return memo[(m, n)]
 
 
