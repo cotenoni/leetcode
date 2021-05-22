0a0efa89f10398011f8dc9b2cbcaeae06bae77b5
cotenoni <nicolas.cotenolin@gmail.com>
2021-05-21 20:43:40 -0400

    bestSum with memoization

--- memoization/bestSum.py
+++ memoization/bestSum.py
@@ -2,31 +2,32 @@ from typing import List
 
 
 class Solution:
-    def bestSum(self, target:int, numbers: List[int]) -> List[int]:
-        if target == 0:
-            return []
+    def bestSum(self, target:int, numbers: List[int], memo = None) -> List[int]:
+        if memo is None:
+            memo = {}
         
-        if target < 0:
-            return None
+        if target in memo: return memo[target]
+        if target == 0: return []
+        if target < 0: return None
 
-        result = []
-
+        shortestCombination = None
         for n in numbers:
             remainder = target - n
-            remainderResult = self.bestSum(remainder, numbers)
+            remainderCombination = self.bestSum(remainder, numbers, memo)
 
-            if remainderResult != None and (len(remainderResult) <= len(result) or len(result) == 0):
-                result = remainderResult
-                result.append(n)
+            if remainderCombination is not None:
+                remainderCombination = remainderCombination.copy()
+                remainderCombination.append(n)
 
-        if len(result) == 0:
-            return None
+                if shortestCombination is None or len(remainderCombination) < len(shortestCombination):
+                    shortestCombination = remainderCombination.copy()
 
-        return result
+        memo[target] = shortestCombination
+        return shortestCombination
 
         
 solution = Solution()
 print(solution.bestSum(7, [5, 3, 4, 7])) #expected: [7]
-print(solution.bestSum(7, [2, 4])) #expected: None
+print(solution.bestSum(8, [1, 4, 5])) #expected: [4, 4]
 print(solution.bestSum(8, [2, 3, 5])) #expected: [3, 5]
-#print(solution.bestSum(300, [7, 14])) #expected: None
+print(solution.bestSum(100, [1, 2, 5, 25])) #expected: [25, 25, 25, 25]
