c7da5b8d254ebec08ffb7b3c9264fb838b7e17ea
cotenoni <nicolas.cotenolin@gmail.com>
2021-05-24 20:20:42 -0400

    countConstruct memoization

--- memoization/canConstruct.py
+++ memoization/canConstruct.py
@@ -13,7 +13,7 @@ class Solution:
             if target.startswith(word):
                 remainingTarget = target[len(word):]
 
-                if self.canConstruct(remainingTarget, wordBank):
+                if self.canConstruct(remainingTarget, wordBank, memo):
                     memo[target] = True
                     return True
 
@@ -25,4 +25,4 @@ solution = Solution()
 print(solution.canConstruct("", ["cat", "dog", "mouse"])) #expected: true
 print(solution.canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])) #expected: true
 print(solution.canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])) #expected: false
-print(solution.canConstruct("eeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeee"])) #expected: false
+print(solution.canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeee", "eeeee"])) #expected: false
---
+++ memoization/countConstruct.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def countConstruct(self, target:str, wordBank: List[str], memo = None) -> bool:
+        if memo == None:
+            memo = {}
+
+        if target in memo: return memo[target]
+        if target == "": return 1
+
+        constructCount = 0
+        for word in wordBank:
+            if target.startswith(word):
+                suffix = target[len(word):]
+                constructCount += self.countConstruct(suffix, wordBank, memo)
+
+        memo[target] = constructCount            
+        return constructCount
+                
+        
+solution = Solution()
+print(solution.countConstruct("", ["cat", "dog", "mouse"])) #expected: 1
+print(solution.countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])) #expected: 1
+print(solution.countConstruct("skateboard", ["sk", "ateboard", "skate", "board", "ska", "teboard", "boar"])) #expected: 3
+print(solution.countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeee", "eeeee"])) #expected: 0
