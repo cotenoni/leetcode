49fb6513e984a56daf9d5aed01dc321f790e3f06
cotenoni <nicolas.cotenolin@gmail.com>
2021-05-31 13:35:52 -0400

    Linked List Cycle with mutable node (kinda cheating here)

---
+++ LinkedList/linkedListCycle.js
@@ -0,0 +1,50 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/773/
+
+/**
+ * Definition for singly-linked list.
+ */
+ function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+ 
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+ var hasCycle = function(head) {
+    if (!head || !head.next) return false;
+
+    let pos = 0;
+    let currentNode = head;
+
+    while (currentNode.next) {
+        if (typeof currentNode.pos !== "undefined") return true;
+
+        currentNode["pos"] = pos;
+        pos++;
+        currentNode = currentNode.next;
+    }
+
+    return false;
+};
+
+
+// console.log(hasCycle(null)); // false
+
+// let head = new ListNode(1);
+// console.log(hasCycle(head)); // false
+
+
+// let second = new ListNode(2)
+// let head = new ListNode(1, second);
+// second.next = head;
+// console.log(hasCycle(head)); // true
+
+let fourth = new ListNode(-4);
+let third = new ListNode(0, fourth);
+let second = new ListNode(2, third)
+let head = new ListNode(3, second);
+fourth.next = second;
+console.log(hasCycle(head)); // true
+
--- LinkedList/reverseList.js
+++ LinkedList/reverseList.js
@@ -7,7 +7,7 @@
  * }
  */
 
- function ListNode(val, next) {
+function ListNode(val, next) {
     this.val = (val===undefined ? 0 : val)
     this.next = (next===undefined ? null : next)
 }
