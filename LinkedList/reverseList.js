d71c181cd954eac0d2f832b022ea98300ab0d36f
cotenoni <nicolas.cotenolin@gmail.com>
2021-05-31 11:15:09 -0400

    reverseList accepted solution

--- LinkedList/reverseList.js
+++ LinkedList/reverseList.js
@@ -16,6 +16,36 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
- var reverseList = function(head) {
-    
+const reverseList = (head) => {
+    if (!head || !head.next) return head;
+
+    let currentNode = head;
+    let nextNode = head.next;
+
+    while (nextNode) {
+        const tempNext = nextNode.next;
+        const tempCurrent = nextNode;
+
+        nextNode.next = currentNode;
+        nextNode = tempNext;
+        currentNode = tempCurrent;
+    }
+
+    head.next = null;
+    return currentNode;
 };
+
+let fifth = new ListNode(5);
+let fourth = new ListNode(4, fifth);
+let third = new ListNode(3, fourth);
+let second = new ListNode(2, third);
+let head = new ListNode(1, second);
+console.log(reverseList(head)); // [5, 4, 3, 2, 1];
+
+
+// let second = new ListNode(2);
+// let head = new ListNode(1, second);
+// console.log(reverseList(head)); // [2, 1];
+
+
+//console.log(reverseList(null)); // [];
