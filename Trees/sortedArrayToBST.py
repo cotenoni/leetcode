4696269260133ad4bbb4b00fbd6dab98ffd359a1
cotenoni <nicolas.cotenolin@gmail.com>
2021-06-07 10:30:17 -0400

    Removed unused function

--- Trees/sortedArrayToBST.py
+++ Trees/sortedArrayToBST.py
@@ -9,19 +9,6 @@ class TreeNode:
 
     def __repr__(self):
         return 'TreeNode({})'.format(self.val)
-
-def deserialize(string):
-    if string == '{}':
-        return None
-    nodes = [None if val == 'null' else TreeNode(int(val))
-             for val in string.strip('[]{}').split(',')]
-    kids = nodes[::-1]
-    root = kids.pop()
-    for node in nodes:
-        if node:
-            if kids: node.left  = kids.pop()
-            if kids: node.right = kids.pop()
-    return root       
  
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
