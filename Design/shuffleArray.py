f3f42d7aa7f41463ec413827343e3de5ecd20332
cotenoni <nicolas.cotenolin@gmail.com>
2021-06-09 09:15:36 -0400

    Shuffle An Array using Fisher-Yates Algorithm

--- Design/shuffleArray.py
+++ Design/shuffleArray.py
@@ -1,29 +1,34 @@
+from hashlib import new
 from typing import List
 import random
 
 class Solution:
 
     def __init__(self, nums: List[int]):
-        self.originalArray = nums
+        self.original_array = nums
 
     def reset(self) -> List[int]:
         """
         Resets the array to its original configuration and return it.
         """
-        return self.originalArray
+        return self.original_array
 
     def shuffle(self) -> List[int]:
         """
         Returns a random shuffling of the array.
         """
-        newArray = self.originalArray.copy()
-        random.shuffle(newArray)
+        new_array = self.original_array.copy()
+        
+        for i in range(len(new_array)):
+            random_idx = random.randrange(i, len(new_array))
+            new_array[i], new_array[random_idx] = new_array[random_idx], new_array[i]
 
-        return newArray
+        return new_array
 
 
 # Your Solution object will be instantiated and called as such:
 obj = Solution([1, 2, 3])
 print(obj.shuffle())
 print(obj.reset())
+print(obj.shuffle())
 print(obj.shuffle())
