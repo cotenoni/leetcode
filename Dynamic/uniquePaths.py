3783bf112bb63e59b822a78fbbdecd6db095bfbe
cotenoni <nicolas.cotenolin@gmail.com>
2021-06-10 13:47:11 -0400

    Unique Path using tabulation

---
+++ Dynamic/uniquePaths.py
@@ -0,0 +1,24 @@
+#https://leetcode.com/explore/interview/card/top-interview-questions-medium/111/dynamic-programming/808/
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # m = number of lines
+        # n = number of columns
+        grid = [ [0]*n for i in range(m)]
+        grid[0][0] = 1
+        
+        for i in range(m):
+            for j in range(n):
+                currentCoordinateWays = grid[i][j]
+                if i + 1 < m:
+                    grid[i + 1][j] += currentCoordinateWays
+                
+                if j + 1 < n:
+                    grid[i][j+1] += currentCoordinateWays
+
+        return grid[m - 1][n - 1]
+            
+
+solution = Solution()
+print(solution.uniquePaths(3, 7)) #28
+print(solution.uniquePaths(3, 2)) #3
+print(solution.uniquePaths(3, 3)) #6
