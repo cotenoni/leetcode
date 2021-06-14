ec936950b09a0ba2de3504cf709694add3a36dbc
cotenoni <nicolas.cotenolin@gmail.com>
2021-06-13 20:01:24 -0400

    Rotate array (+k position) using reversing method

--- Array/rotate_array.py
+++ Array/rotate_array.py
@@ -1,30 +1,28 @@
 from typing import List
 
+#https://leetcode.com/problems/rotate-array/
 class Solution:
+    def reverse(self, nums, start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        length = len(nums)
-        numsCopy = nums.copy()
-        newIndex = 0
-        
-        for i in range(length):
-            newIndex = i
+        n = len(nums)
+        k %= n
 
-            for j in range(k):
-                if newIndex < length - 1:
-                    newIndex += 1
-                else:
-                    newIndex = 0
-
-            nums[newIndex] = numsCopy[i]
-
-        print(nums)
+        self.reverse(nums, 0, n - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, n - 1)
         
 
 solution = Solution()
-solution.rotate([1,2,3,4,5,6,7], 3)
-solution.rotate([1,2], 3)
+solution.rotate([1,2,3,4,5,6,7], 3) #[5,6,7,1,2,3,4]
+solution.rotate([-1,-100,3,99], 2) #[3,99,-1,-100]
+solution.rotate([1,2], 3) #[2, 1]
 
 
