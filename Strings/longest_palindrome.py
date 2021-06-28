dd1153f48d9ce48b05233ed86fcd18bc4e5f1048
cotenoni <nicolas.cotenolin@gmail.com>
2021-06-28 10:36:14 -0400

    Longest Palindrome using expand from the middle algo

--- Strings/longest_palindrome.py
+++ Strings/longest_palindrome.py
@@ -2,37 +2,37 @@
 #https://leetcode.com/explore/interview/card/top-interview-questions-medium/103/array-and-strings/780/
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def is_palindrome(s: str, memo = None):
-            if memo is None:
-                memo = {}
+        def expandFromMiddle(s: str, left: int, right: int) -> int:
+            if s is None or left > right:
+                return 0
+
+            while left >= 0 and right < len(s) and s[left].lower() == s[right].lower():
+                left -= 1
+                right += 1
             
-            if s in memo:
-                return memo[s]
+            return right - left - 1
 
-            if len(s) == 0 or len(s) == 1:
-                return True
+        
+        start, end = 0, 0
 
-            ret = False
-            if s[0].lower() == s[len(s) - 1].lower():
-                ret = is_palindrome(s[1:len(s)-1], memo)
+        for i in range(len(s)):
+            length1 = expandFromMiddle(s, i, i)
+            length2 = expandFromMiddle(s, i, i + 1)
+            max_length = max(length1, length2)
 
-            memo[s] = ret
-            return ret
+            if max_length > end - start:
+                start = i - ((max_length - 1) // 2)
+                end = i + (max_length // 2)
 
-        memo = {}
-        longest_palindrome = ""
-        for i in range(len(s) + 1):
-            for j in range(len(s), i, -1):
-                str_to_evaluate = s[i:j]
-               
-                if is_palindrome(str_to_evaluate, memo) and len(str_to_evaluate) > len(longest_palindrome):
-                    longest_palindrome = str_to_evaluate
+        return s[start:end + 1]
 
-        return longest_palindrome
+
+
             
 
 
 solution = Solution()
+print(solution.longestPalindrome("racecar")) #bab or aba
 print(solution.longestPalindrome("babad")) #bab or aba
 print(solution.longestPalindrome("cbbd")) #bb
 print(solution.longestPalindrome("a")) #a
