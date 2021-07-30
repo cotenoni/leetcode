c92cdf71283e622eff7edf11d4e882ec1e30dca4
cotenoni <nicolas.cotenolin@gmail.com>
2021-07-30 10:49:16 -0400

    Increasing Triplet Subsquence in O(n) time and O(1) space

--- Dynamic/increasing_triplet_subsequence.py
+++ Dynamic/increasing_triplet_subsequence.py
@@ -6,19 +6,21 @@ class Solution:
         if len(nums) < 3:
             return False
 
-        table = [1] * len(nums)
-        
-        for i in range(1, len(nums)):
-            for j in range(i - 1, -1, -1):
-                if nums[j] < nums[i]:
-                    table[i] = max(table[i], table[j] + 1)
-        
-        return max(table) >= 3
+        i, j, k = nums[0], float("inf"), float("inf")
+        for num in nums:
+            if num <= i:
+                i = num
+            elif num <= j:
+                j = num
+            else:
+                k = num
+                
+        return j != float("inf") and k != float("inf")
         
 
 
 sol = Solution()
-# print(sol.increasingTriplet([1,2]) == False)
+print(sol.increasingTriplet([1,2]) == False)
 print(sol.increasingTriplet([1,2,3,4,5]) == True)
 print(sol.increasingTriplet([5,4,3,2,1]) == False)
 print(sol.increasingTriplet([2,1,5,0,4,6]) == True)
