5c557242a6f2fb89496f93eb7ec2a075fb072a4f
cotenoni <nicolas.cotenolin@gmail.com>
2021-08-23 14:44:00 -0400

    Missing number in O(n) time and O(1) space

--- Array/missing_number.py
+++ Array/missing_number.py
@@ -3,8 +3,14 @@ from typing import List
 #https://leetcode.com/problems/missing-number/
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        target_numbers = set(range(len(nums) + 1))
-        return (target_numbers - set(nums)).pop()
+        n = len(nums)
+        target_sum = n * (n + 1) // 2
+
+        nums_sum = 0
+        for num in nums:
+            nums_sum += num
+        
+        return target_sum - nums_sum
 
 sol = Solution()
 print(sol.missingNumber([3,0,1]) == 2)
