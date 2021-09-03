82e7128a0659f3eb37a7c823ced3fcc6e2ce2e2a
cotenoni <nicolas.cotenolin@gmail.com>
2021-09-03 10:15:06 -0400

    Merge Sorted Array in O(m + n) time and O(1) space

--- Array/merge_sorted_arrays.py
+++ Array/merge_sorted_arrays.py
@@ -6,23 +6,27 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        sorted_array = []
+        i = m -1 
+        j = n - 1
+        end = m + n - 1
 
-        i = j = 0
-        while i < m or j < n:
-            if i < m and (j >= n or nums1[i] < nums2[j]):
-                sorted_array.append(nums1[i])
-                i += 1
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[end] = nums1[i]
+                i -= 1
             else:
-                sorted_array.append(nums2[j])
-                j += 1
-        
-        for i, num in enumerate(sorted_array):
-            nums1[i] = num
+                nums1[end] = nums2[j]
+                j -= 1
+    
+            end -=1
+        print(nums1)
+
 
 
 
 sol = Solution()
 sol.merge([1,2,3,0,0,0], 3, [2,5,6], 3) #1,2,2,3,5,6
+sol.merge([5,6,7,0,0,0], 3, [1,2,3], 3) #1,2,3,5,6,7
+sol.merge([5,6,7,0,0,0], 3, [1,2,7], 3) #1,2,5,6,7,7
 sol.merge([1], 1, [], 0) #1
 sol.merge([0], 0, [1], 1) #1
