2801a8403a41a09de7c119b9e0989de60e61ca8f
cotenoni <nicolas.cotenolin@gmail.com>
2021-09-08 15:52:42 -0400

    Power of Three using recursion in O(log3 n) time and O(1) space

--- Math/power_of_three.py
+++ Math/power_of_three.py
@@ -2,14 +2,13 @@
 #https://leetcode.com/problems/power-of-three/
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        for i in range(n):
-            if 3**i == n:
-                return True
+        if n < 1:
+            return False
 
-            if 3**i > n:
-                return False
+        while n % 3 == 0:
+            n //= 3
 
-        return False
+        return n == 1
 
 
 sol = Solution()
@@ -17,3 +16,4 @@ print(sol.isPowerOfThree(0) == False)
 print(sol.isPowerOfThree(27) == True)
 print(sol.isPowerOfThree(9) == True)
 print(sol.isPowerOfThree(45) == False)
+print(sol.isPowerOfThree(2))
