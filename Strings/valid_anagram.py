f7540c1880c1f816b2961b79eeffd973d40f4836
cotenoni <nicolas.cotenolin@gmail.com>
2021-09-10 15:07:25 -0400

    Another way to do valid anagram in O(s+t) time and O(s+t) space

--- Strings/valid_anagram.py
+++ Strings/valid_anagram.py
@@ -3,13 +3,25 @@ import collections
 #https://leetcode.com/problems/valid-anagram/
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_counts = collections.Counter(s)
-        t_counts = collections.Counter(t)
+        s_counts, t_counts = collections.defaultdict(int), collections.defaultdict(int)
 
-        return len(s_counts - t_counts) == 0 and len(t_counts - s_counts) == 0
+        for i in range(len(s)):
+            s_counts[s[i]] += 1
 
+        for i in range(len(t)):
+            t_counts[t[i]] += 1
+
+        if len(t_counts) > len(s_counts):
+            s_counts, t_counts = t_counts, s_counts
+
+        for key, value in s_counts.items():
+            if key not in t_counts or t_counts[key] != value:
+                return False
+
+        return True
+        
 
 sol = Solution()
-# print(sol.isAnagram("anagram","nagaram") == True)
-# print(sol.isAnagram("rat", "car") == False)
+print(sol.isAnagram("anagram","nagaram") == True)
+print(sol.isAnagram("rat", "car") == False)
 print(sol.isAnagram("a", "ab") == False)
